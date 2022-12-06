e35d090776279955854071cdba7ff84291302d28
Jon R <jmrapp1270@gmail.com>
2022-12-05 19:22:09 -0500

    Publish to Maven Central

--- API/build.gradle.kts
+++ API/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm")
     kotlin("plugin.serialization") version "1.6.20"
     `maven-publish`
+    signing
 }
 
 repositories {
@@ -19,21 +20,50 @@ dependencies {
 
 publishing {
     publications {
-        create<MavenPublication>("maven") {
+        create<MavenPublication>("mavenJava") {
             groupId = "io.matchedup"
             version = version
             artifactId = project.name.toLowerCase()
             from(components["java"])
-        }
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/MatchedUp/MatchedUpSdk")
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
+            pom {
+                name.set("MatchedUp SDK")
+                description.set("The SDK for MatchedUp")
+                url.set("https://matchedup.io")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://github.com/MatchedUp/MatchedUpSdk/blob/main/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jmrapp1")
+                        name.set("Jon Rapp")
+                        email.set("jmrapp1270@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/MatchedUp/MatchedUpSdk.git")
+                    url.set("https://github.com/MatchedUp/MatchedUpSdk")
                 }
             }
         }
     }
+    repositories {
+        if (version.toString().endsWith("SNAPSHOT")) {
+            maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") {
+                name = "sonatypeReleaseRepository"
+                credentials(PasswordCredentials::class)
+            }
+        } else {
+            maven("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") {
+                name = "sonatypeSnapshotRepository"
+                credentials(PasswordCredentials::class)
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
